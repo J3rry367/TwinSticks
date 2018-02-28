e11a05a233fb95c8e17eba9c8b4a603aff2be4a2
Jerry <jerry.johnson367@gmail.com>
2018-02-28 08:38:10 -0800

    Added replay system Need to fix buffer size so it only plays frames that have been recorded when buffer has not been filled

---
+++ Assets/GameManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class GameManager : MonoBehaviour {
+
+    public bool isRecording = true;
+
+	void Update () {
+        if (CrossPlatformInputManager.GetButton("Fire1"))
+        {
+            isRecording = false;
+            Debug.Log("Playback mode");
+        } else
+        {
+            isRecording = true;
+        }
+	}
+}
